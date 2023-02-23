5a473064561e8640c05d2f3ed7f9ebf38ae3e75a
Pobitrodeb <pobitrodn@gmail.com>
2023-02-23 22:50:24 +0600

    user and role relationship

--- database/migrations/2023_02_23_164012_create_roles_table.php
+++ database/migrations/2023_02_23_164012_create_roles_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->inter('role_id')->default(3);
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar')->default('user.png');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
